611f2024a218b3da45986698baefbca1526adc5d
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-21 11:15:17 -0600

    Fix spelling error

--- src/components/FeaturedWork.js
+++ src/components/FeaturedWork.js
@@ -4,7 +4,7 @@ import '../css/FeaturedWork.css';
 function FeaturedWork() {
   return(
     <div class="featured-work">
-      <b><div class="featured-work-header">FEATUED WORK</div></b>
+      <b><div class="featured-work-header">FEATURED WORK</div></b>
     </div>
   )
 }
