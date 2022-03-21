d764f5e5e9226c588800d66a17c0d3e763857488
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-21 00:41:24 -0600

    Complete responsive navbar without animated styling

--- src/components/NavigateBar.js
+++ src/components/NavigateBar.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/NavigateBar.css';
+import ReorderIcon from '@material-ui/icons/Reorder';
 
 import user_logo from './images/user_logo.png';
 import dragon_logo from './images/dragon_logo.png';
 import shopping_cart from './images/shopping_cart.png';
 
 function NavigateBar() {
+
+  const [showLinks, setShowLinks] = useState(false);
+
   return(
-    <nav className="navigation-bar">
-      <a className="navbar-img-left" href="/"><img src={dragon_logo} height="40" alt="tofu logo"/></a>
-      <a className="navbar-img-right" href="/"><img src={user_logo} height="25" alt="profile"/></a>
-      <a className="navbar-img-right" href="/cart"><img src={shopping_cart} height="25" alt="shopping cart"/></a>
-      <b><a className="navbar-link" href="/">CONTACT</a></b>
-      <b><a className="navbar-link" href="/">COMMISSIONS</a></b>
-      <b><a className="navbar-link" href="/">SHOP</a></b>
-      <b><a className="navbar-link" href="/">FEATURED</a></b>
-      <b><a className="navbar-link" href="/">HOME</a></b>
-    </nav>
+    <div className="NavigateBar">
+      <div className="left-side">
+        <div className="images">
+          <a href="/cart"><img src={dragon_logo} alt="tofu-logo"/></a>
+        </div>
+      </div>
+      <div className="right-side">
+        <div className="links" id={ showLinks ? "hidden" : "" }>
+          <b><a href="/">HOME</a></b>
+          <b><a href="/">FEATURED</a></b>
+          <b><a href="/">SHOP</a></b>
+          <b><a href="/">COMMISSIONS</a></b>
+          <b><a href="/">CONTACT</a></b>
+        </div>
+        <button onClick={() => setShowLinks(!showLinks)}> <ReorderIcon /></button>
+        <div className="images">
+          <a href="/cart"><img src={shopping_cart} alt="cart"/></a>
+          <a href="/"><img src={user_logo} alt="profile"/></a>
+        </div>
+      </div>
+    </div>
   );
 };
 
