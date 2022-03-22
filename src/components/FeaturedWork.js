d3c908f47bf0ae3eb3ae546b23c546eeb996f89f
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-21 21:08:55 -0600

    Add auto play

--- src/components/FeaturedWork.js
+++ src/components/FeaturedWork.js
@@ -11,7 +11,7 @@ function FeaturedWork() {
   return(
     <div className="featured-work">
       <div className="title">FEATURED</div>
-      <div className="main-carousel" data-flickity='{ "wrapAround": true, "imagesLoaded": true, "groupCells": 1 }'>
+      <div className="main-carousel" data-flickity='{ "wrapAround": true, "imagesLoaded": true, "groupCells": 1, "autoPlay": 7000 }'>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
