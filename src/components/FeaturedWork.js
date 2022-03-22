4b8fd62f8a66c4677c0ffe0abdda2d5f03aa8fd0
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-21 21:24:47 -0600

    Update Featured Section

--- src/components/FeaturedWork.js
+++ src/components/FeaturedWork.js
@@ -10,7 +10,7 @@ import feat1 from '../images/featured/feat1.png';
 function FeaturedWork() {
   return(
     <div className="featured-work">
-      <div className="title">FEATURED</div>
+      <div className="title">FEATURED WORK</div>
       <div className="main-carousel" data-flickity='{ "wrapAround": true, "imagesLoaded": true, "groupCells": 1, "autoPlay": 7000 }'>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
