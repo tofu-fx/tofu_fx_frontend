ca48ef13d824c2203fbdf21c26fa0eafba10a2d8
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-22 09:14:05 -0600

    Add featured images

--- src/components/FeaturedWork.js
+++ src/components/FeaturedWork.js
@@ -6,19 +6,31 @@ import '../css/FeaturedWork.css';
 
 // Import Featured
 import feat1 from '../images/featured/feat1.jpg';
+import feat2 from '../images/featured/feat2.jpg';
+import feat3 from '../images/featured/feat3.jpg';
+import feat4 from '../images/featured/feat4.jpg';
+import feat5 from '../images/featured/feat5.jpg';
+import feat6 from '../images/featured/feat6.jpg';
+import feat7 from '../images/featured/feat7.jpg';
+import feat8 from '../images/featured/feat8.jpg';
+import feat9 from '../images/featured/feat9.jpg';
+import feat10 from '../images/featured/feat10.jpg';
 
 function FeaturedWork() {
   return(
     <div className="featured-work">
       <div className="title">FEATURED WORK</div>
       <div className="main-carousel" data-flickity='{ "wrapAround": true, "imagesLoaded": true, "groupCells": 1, "autoPlay": 7000 }'>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
-        <img src={feat1} alt="tulip" />
+        <img src={feat1} alt="featured_work" />
+        <img src={feat2} alt="featured_work" />
+        <img src={feat3} alt="featured_work" />
+        <img src={feat4} alt="featured_work" />
+        <img src={feat5} alt="featured_work" />
+        <img src={feat6} alt="featured_work" />
+        <img src={feat7} alt="featured_work" />
+        <img src={feat8} alt="featured_work" />
+        <img src={feat9} alt="featured_work" />
+        <img src={feat10} alt="featured_work" />
       </div>
     </div>
   )
