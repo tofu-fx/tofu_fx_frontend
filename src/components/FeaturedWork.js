290dee806f778d6f1607250292a8d0cfce212988
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-21 20:45:52 -0600

    Featured Section Complete (needs more styling)

--- src/components/FeaturedWork.js
+++ src/components/FeaturedWork.js
@@ -4,10 +4,22 @@ import React from 'react';
 // Import Styling
 import '../css/FeaturedWork.css';
 
+// Import Featured
+import feat1 from '../images/featured/feat1.png';
+
 function FeaturedWork() {
   return(
     <div className="featured-work">
       <div className="title">FEATURED</div>
+      <div className="main-carousel" data-flickity='{ "wrapAround": true, "imagesLoaded": true, "groupCells": 1 }'>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
+        <img src={feat1} alt="tulip" />
+      </div>
     </div>
   )
 }
