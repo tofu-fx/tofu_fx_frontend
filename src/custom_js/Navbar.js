c38a9543185e3a76f76243f0a44609c5eff43649
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-23 17:48:10 -0600

    Add navbar_height

--- src/custom_js/Navbar.js
+++ src/custom_js/Navbar.js
@@ -1,8 +1,11 @@
 export default window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
+  var currentScrollPos = window.pageYOffset;
+
+  var navbar_height = document.querySelector('.NavigateBar').offsetHeight;
+
   if (currentScrollPos < 680) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-80px";
+    document.getElementById("navbar").style.top = `-${navbar_height}px`;
   }
 }
