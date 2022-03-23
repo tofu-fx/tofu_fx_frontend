f23ce4df627ae8d83cfb6e26e8f6f8679da86995
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-23 17:01:09 -0600

    Edit navbar dissapear page length paramater

--- src/custom_js/Navbar.js
+++ src/custom_js/Navbar.js
@@ -1,6 +1,6 @@
 export default window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-  if (currentScrollPos < 600) {
+  if (currentScrollPos < 680) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-80px";
