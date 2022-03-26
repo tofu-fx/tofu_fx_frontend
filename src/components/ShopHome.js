47b75853032b09f8c4142fd691bf849374cfc9d2
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-25 23:03:49 -0600

    Finish Shop Cards?

--- src/components/ShopHome.js
+++ src/components/ShopHome.js
@@ -3,7 +3,12 @@ import '../css/ShopHome.css';
 
 import ShopCategoryCard from './ShopCategoryCard.js';
 
-import example_card_background from '../images/shop_categories/testing.jpg';
+import banners_category from '../images/shop_categories/banners.jpg';
+import headers_category from '../images/shop_categories/headers.jpg';
+import logos_category from '../images/shop_categories/logos.jpg';
+import streaming_graphics_category from '../images/shop_categories/streaming_graphics.jpg';
+import thumbnails_category from '../images/shop_categories/thumbnails.jpg';
+import wallpapers_category from '../images/shop_categories/wallpapers.jpg';
 
 
 function ShopHome() {
@@ -12,12 +17,12 @@ function ShopHome() {
       <div className="title"><div className="text">SHOP</div></div>
       <div className="categories">
         <div className="inner">
-          <ShopCategoryCard card_name="YOUTUBE GRAPHICS" background={example_card_background} href="/shop/youtube"/>
-          <ShopCategoryCard card_name="STREAMING GRAPHICS" background={example_card_background} href="/shop/streaming_graphics"/>
-          <ShopCategoryCard card_name="CATEGORY NAME HERE" background={example_card_background} href="/shop/youtube"/>
-          <ShopCategoryCard card_name="SOMETHING!@#$" background={example_card_background} href="/shop/youtube"/>
-          <ShopCategoryCard card_name="AVATARS" background={example_card_background} href="/shop/youtube"/>
-          <ShopCategoryCard card_name="CATEGORY TITLE THAT TAKES MULTIPLE LINES" background={example_card_background} href="/shop/youtube"/>
+          <ShopCategoryCard card_name="BANNERS" background={banners_category} href="/shop/banners"/>
+          <ShopCategoryCard card_name="HEADERS" background={headers_category} href="/shop/headers"/>
+          <ShopCategoryCard card_name="LOGOS" background={logos_category} href="/shop/logos"/>
+          <ShopCategoryCard card_name="STREAMING GRAPHICS" background={streaming_graphics_category} href="/shop/streaming_graphics"/>
+          <ShopCategoryCard card_name="THUMBNAILS" background={thumbnails_category} href="/shop/thumbnails"/>
+          <ShopCategoryCard card_name="WALLPAPERS" background={wallpapers_category} href="/shop/wallpapers"/>
         </div>
       </div>
     </div>
