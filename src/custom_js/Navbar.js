21f37a94af5fe984171f807c77f918a3c6ff0cbd
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-25 18:46:00 -0600

    Remove the ability for navbar to dissapear on other pages

--- src/custom_js/Navbar.js
+++ src/custom_js/Navbar.js
@@ -1,7 +1,7 @@
 export default window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
 
-  var navbar_height = document.querySelector('.NavigateBar').offsetHeight;
+  var navbar_height = document.querySelector('.home-page .NavigateBar').offsetHeight;
 
   if (currentScrollPos < 650) {
     document.getElementById("navbar").style.top = "0";
