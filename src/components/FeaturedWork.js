207e4510037e9af0413b2afcc3bbe8fed69a4ff7
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-30 16:45:16 -0600

    .

--- src/components/FeaturedWork.js
+++ src/components/FeaturedWork.js
@@ -20,20 +20,22 @@ class FeaturedWork extends React.Component {
   }
 
   render() {
+    const image_urls = this.state.image_urls
+    
     return(
       <div className="featured-work">
         <div className="title"><div className="text">FEATURED WORK</div></div>
         <div className="main-carousel" data-flickity='{ "wrapAround": true, "imagesLoaded": true, "groupCells": 1, "autoPlay": 7000 }'>
-          <img src={this.state.image_urls[0]} alt="featured-1"/>
-          <img src={this.state.image_urls[1]} alt="featured-2"/>
-          <img src={this.state.image_urls[2]} alt="featured-3"/>
-          <img src={this.state.image_urls[3]} alt="featured-4"/>
-          <img src={this.state.image_urls[4]} alt="featured-5"/>
-          <img src={this.state.image_urls[5]} alt="featured-6"/>
-          <img src={this.state.image_urls[6]} alt="featured-7"/>
-          <img src={this.state.image_urls[7]} alt="featured-8"/>
-          <img src={this.state.image_urls[8]} alt="featured-9"/>
-          <img src={this.state.image_urls[9]} alt="featured-10"/>
+          <img src={image_urls[0]} alt="featured-1"/>
+          <img src={image_urls[1]} alt="featured-2"/>
+          <img src={image_urls[2]} alt="featured-3"/>
+          <img src={image_urls[3]} alt="featured-4"/>
+          <img src={image_urls[4]} alt="featured-5"/>
+          <img src={image_urls[5]} alt="featured-6"/>
+          <img src={image_urls[6]} alt="featured-7"/>
+          <img src={image_urls[7]} alt="featured-8"/>
+          <img src={image_urls[8]} alt="featured-9"/>
+          <img src={image_urls[9]} alt="featured-10"/>
         </div>
         <div className="button-container">
           <form action="https://www.behance.net/cozmoxxix" target="_blank" rel="noopener noreferrer">
