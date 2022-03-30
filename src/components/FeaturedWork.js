c2d11a60ccb7eeaf5264fc816574ab239a6dcc6b
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-30 17:19:55 -0600

    Revert featured images to store locally

--- src/components/FeaturedWork.js
+++ src/components/FeaturedWork.js
@@ -1,50 +1,45 @@
 // Import React
 import React from 'react';
-import axios from 'axios';
 
 // Import Styling
 import '../css/FeaturedWork.css';
 import '../css/external/button_animations.css';
 
-class FeaturedWork extends React.Component {
-  state = {
-    image_urls: []
-  }
+// Import Featured
+import feat1 from '../images/featured/feat1.jpg';
+import feat2 from '../images/featured/feat2.jpg';
+import feat3 from '../images/featured/feat3.jpg';
+import feat4 from '../images/featured/feat4.jpg';
+import feat5 from '../images/featured/feat5.jpg';
+import feat6 from '../images/featured/feat6.jpg';
+import feat7 from '../images/featured/feat7.jpg';
+import feat8 from '../images/featured/feat8.jpg';
+import feat9 from '../images/featured/feat9.jpg';
+import feat10 from '../images/featured/feat10.jpg';
 
-  componentDidMount() {
-    axios.get(`https://tofufx-backend.herokuapp.com/featured_work?api_key=${process.env.REACT_APP_TOFU_BACKEND_API_KEY}`)
-      .then(response => {
-        const image_urls = response.data.data.featured_images;
-        this.setState({ image_urls });
-     })
-  }
-
-  render() {
-    const image_urls = this.state.image_urls
-
-    return(
-      <div className="featured-work">
-        <div className="title"><div className="text">FEATURED WORK</div></div>
-        <div className="main-carousel" data-flickity='{ "wrapAround": true, "imagesLoaded": true, "groupCells": 1, "autoPlay": 7000 }'>
-          <img src={image_urls[0]} alt="featured-1"/>
-          <img src={image_urls[1]} alt="featured-2"/>
-          <img src={image_urls[2]} alt="featured-3"/>
-          <img src={image_urls[3]} alt="featured-4"/>
-          <img src={image_urls[4]} alt="featured-5"/>
-          <img src={image_urls[5]} alt="featured-6"/>
-          <img src={image_urls[6]} alt="featured-7"/>
-          <img src={image_urls[7]} alt="featured-8"/>
-          <img src={image_urls[8]} alt="featured-9"/>
-          <img src={image_urls[9]} alt="featured-10"/>
-        </div>
-        <div className="button-container">
-          <form action="https://www.behance.net/cozmoxxix" target="_blank" rel="noopener noreferrer">
-            <button className="button button--pan"><span>VIEW FULL PORTFOLIO</span></button>
-          </form>
-        </div>
+function FeaturedWork() {
+  return(
+    <div className="featured-work">
+      <div className="title"><div className="text">FEATURED WORK</div></div>
+      <div className="main-carousel" data-flickity='{ "wrapAround": true, "imagesLoaded": true, "groupCells": 1, "autoPlay": 7000 }'>
+        <img src={feat1} alt="featured_work" />
+        <img src={feat2} alt="featured_work" />
+        <img src={feat3} alt="featured_work" />
+        <img src={feat4} alt="featured_work" />
+        <img src={feat5} alt="featured_work" />
+        <img src={feat6} alt="featured_work" />
+        <img src={feat7} alt="featured_work" />
+        <img src={feat8} alt="featured_work" />
+        <img src={feat9} alt="featured_work" />
+        <img src={feat10} alt="featured_work" />
       </div>
-    )
-  }
+      <div className="button-container">
+        <form action="https://www.behance.net/cozmoxxix" target="_blank" rel="noopener noreferrer">
+          <button className="button button--pan"><span>VIEW FULL PORTFOLIO</span></button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default FeaturedWork;
