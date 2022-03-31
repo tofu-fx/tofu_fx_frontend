3dc67ffee129eab8631a426506bcb54968716411
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-30 18:18:18 -0600

    Fix View Full Portfolio button

--- src/components/FeaturedWork.js
+++ src/components/FeaturedWork.js
@@ -1,5 +1,6 @@
 // Import React
 import React from 'react';
+import axios from 'axios';
 
 // Import Styling
 import '../css/FeaturedWork.css';
@@ -17,29 +18,43 @@ import feat8 from '../images/featured/feat8.jpg';
 import feat9 from '../images/featured/feat9.jpg';
 import feat10 from '../images/featured/feat10.jpg';
 
-function FeaturedWork() {
-  return(
-    <div className="featured-work">
-      <div className="title"><div className="text">FEATURED WORK</div></div>
-      <div className="main-carousel" data-flickity='{ "wrapAround": true, "imagesLoaded": true, "groupCells": 1, "autoPlay": 7000 }'>
-        <img src={feat1} alt="featured_work" />
-        <img src={feat2} alt="featured_work" />
-        <img src={feat3} alt="featured_work" />
-        <img src={feat4} alt="featured_work" />
-        <img src={feat5} alt="featured_work" />
-        <img src={feat6} alt="featured_work" />
-        <img src={feat7} alt="featured_work" />
-        <img src={feat8} alt="featured_work" />
-        <img src={feat9} alt="featured_work" />
-        <img src={feat10} alt="featured_work" />
+class FeaturedWork extends React.Component {
+  state = {
+    behance_link: ""
+  }
+
+  componentDidMount() {
+    axios.get(`https://tofufx-backend.herokuapp.com/socials?api_key=${process.env.REACT_APP_TOFU_BACKEND_API_KEY}`)
+      .then(response => {
+        const behance_link = response.data.behance;
+        this.setState({ behance_link });
+    })
+  }
+
+  render() {
+    return(
+      <div className="featured-work">
+        <div className="title"><div className="text">FEATURED WORK</div></div>
+        <div className="main-carousel" data-flickity='{ "wrapAround": true, "imagesLoaded": true, "groupCells": 1, "autoPlay": 7000 }'>
+          <img src={feat1} alt="featured_work" />
+          <img src={feat2} alt="featured_work" />
+          <img src={feat3} alt="featured_work" />
+          <img src={feat4} alt="featured_work" />
+          <img src={feat5} alt="featured_work" />
+          <img src={feat6} alt="featured_work" />
+          <img src={feat7} alt="featured_work" />
+          <img src={feat8} alt="featured_work" />
+          <img src={feat9} alt="featured_work" />
+          <img src={feat10} alt="featured_work" />
+        </div>
+        <div className="button-container">
+          <form action={this.state.behance_link} target="_blank" rel="noopener noreferrer">
+            <button className="button button--pan"><span>VIEW FULL PORTFOLIO</span></button>
+          </form>
+        </div>
       </div>
-      <div className="button-container">
-        <form action="https://www.behance.net/cozmoxxix" target="_blank" rel="noopener noreferrer">
-          <button className="button button--pan"><span>VIEW FULL PORTFOLIO</span></button>
-        </form>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default FeaturedWork;
