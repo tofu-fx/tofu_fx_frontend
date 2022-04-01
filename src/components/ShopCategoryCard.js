fa198a34a46e247803fa0e4128f7709bfadbe62a
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-04-01 17:02:03 -0600

    Edit hover animation for shop category cards

--- src/components/ShopCategoryCard.js
+++ src/components/ShopCategoryCard.js
@@ -5,7 +5,8 @@ class ShopCategoryCard extends React.Component {
 
   render() {
     return(
-      <a href={this.props.href} className="shop-category-card" style={{backgroundImage: `url(${this.props.background})`}}>
+      <a href={this.props.href} className="shop-category-card">
+        <div className="background" style={{backgroundImage: `url(${this.props.background})`}}></div>
         <div className="card-footer">
           <div className="card-title">
             {this.props.card_name}
